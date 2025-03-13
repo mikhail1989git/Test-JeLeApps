05eef8a6a750e922e8aa3b78d56b1d82f02be2fb
JohnPoher <104150904+JohnPoher@users.noreply.github.com>
2025-03-13 15:40:21 +0400

    addFiles

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from "react";
+
+function App() {
+  return (
+    <div className="container">
+    <div className="square"></div>
+  </div>
+  );
+}
+
+export default App;
